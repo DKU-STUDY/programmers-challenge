80684a1601c87c7607c59727975cf804d6a806bb
junilhwang <tjsdlf4261@naver.com>
2020-08-24 22:38:16 +0900

    refactor: SearchInput 작업 중

--- src/App.refactor.js
+++ src/App.refactor.js
@@ -2,6 +2,8 @@ import { selectOne } from "./utils/index.js";
 import { Cats } from './components/Cats.js';
 import { SearchLoading } from "./components/SearchLoading.js";
 import { Message } from "./components/Message.js";
+import { SearchKeywords } from "./components/SearchKeywords.js";
+import { SearchInput } from "./components/SearchInput.js";
 
 const $keyword = selectOne(".keyword");
 const $keywords = selectOne(".keywords");
@@ -10,5 +12,7 @@ const $searchResults = selectOne(".search-results");
 window.onload = () => {
   new SearchLoading();
   new Message();
+  new SearchInput($keyword);
+  new SearchKeywords($keywords);
   new Cats($searchResults);
 }
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,26 +1,44 @@
+import {debounce, eventBus} from "../utils/index.js";
+
 export const SearchInput = class {
 
-  #state; #target;
+  #target;
 
   constructor (target) {
     this.#target = target;
-    this.setState();
-  }
-
-  #render () {
-
+    this.#event();
   }
 
   #event () {
+    const target = this.#target;
 
+    target.addEventListener("input", ({ target: { value } }) => {
+      if (value.length === 0) return;
+      debounce(() => eventBus.$emit('openRecommend', value), 200);
+    })
+
+    target.addEventListener("keyup", ({ target, key}) => {
+
+      const { value } = target;
+
+      if (['ArrowUp', 'ArrowDown'].includes(key)) {
+        eventBus.$emit('selectKeyword', key === 'ArrowUp' ? -1 : 1);
+      }
+      if (key === 'Escape') {
+        eventBus.$emit('closeRecommend')
+      }
+      if (key === 'Enter') {
+        this.searchCats(value);
+      }
+
+    });
   }
 
-  setState (args) {
-    this.#state = {
-      ...this.#state,
-      ...args
-    };
-    this.#render();
-    this.#event();
+  async searchCats (query) {
+    const isSearchLoading = await new Promise(resolve => eventBus.$emit('getIsSearchLoading', resolve))
+    if (isSearchLoading) return;
+
+    eventBus.$emit('closeRecommend');
+    eventBus.$emit('searchCats', query);
   }
 }

aa5895e568f8b65e8ca2c4d5831cd7957125f0ee
junilhwang <tjsdlf4261@naver.com>
2020-08-24 22:12:21 +0900

    refactor: Message Component 적용 완료

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -41,7 +41,10 @@ export const Cats = class {
       this.#setState({ cats })
     } catch (e) {
       eventBus.$emit('searchLoaded');
-      eventBus.$emit('error', '검색하는 도중 에러가 발생하였습니다.');
+      eventBus.$emit('message', {
+        type: 'error',
+        text: '검색하는 도중 에러가 발생하였습니다.'
+      });
     }
   }
 }

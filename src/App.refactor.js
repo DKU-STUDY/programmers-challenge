d8858bf3a211d70024136582a568948f8248ccb6
junilHwang <tjsdlf4261@naver.com>
2020-08-25 15:09:46 +0900

    fix: App 구조 개선 중 (2)

--- src/App.refactor.js
+++ src/App.refactor.js
@@ -1,24 +1,32 @@
-import { selectOne } from "./utils/index.js";
-import { Cats } from './components/Cats.js';
-import { SearchLoading } from "./components/SearchLoading.js";
-import { Message } from "./components/Message.js";
-import { SearchKeywords } from "./components/SearchKeywords.js";
-import { SearchInput } from "./components/SearchInput.js";
+import {selectOne} from "./utils/index.js";
+import {Cats} from './components/Cats.js';
+import {SearchLoading} from "./components/SearchLoading.js";
+import {Message} from "./components/Message.js";
+import {SearchKeywords} from "./components/SearchKeywords.js";
+import {SearchInput} from "./components/SearchInput.js";
 
 class App {
   #components;
 
   constructor() {
+    const searchMethod = () => this.search();
+    const selectKeyword = () => this.selectKeyword();
+    const searchLoading = new SearchLoading();
+    const searchProps = { searchMethod, selectKeyword };
+    this.#components = {
+      searchLoading,
+      message: new Message(),
+      searchInput: new SearchInput(selectOne(".keyword"), { ...searchProps }),
+      searchKeywords: new SearchKeywords(selectOne(".keywords"), { ...searchProps }),
+      cats: new Cats(selectOne(".search-results"), {
+        loading: searchLoading.loading,
+        loaded: searchLoading.loaded,
+      }),
+    };
+  }
 
-    const $searchInput = selectOne(".keyword");
-    const $searchKeywords = selectOne(".keywords");
-    const $searchResults = selectOne(".search-results");
+  selectKeyword () {
 
-    new SearchLoading();
-    new Message();
-    new SearchInput($searchInput);
-    new SearchKeywords($searchKeywords);
-    new Cats($searchResults);
   }
 
   search () {
